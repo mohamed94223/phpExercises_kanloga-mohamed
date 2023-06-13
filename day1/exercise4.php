d3df92d604a8cbbc98160e0dba352d3eb7eeedc8
mo smock <mohamed.kanloga@gmail.com>
2023-06-13 14:49:14 +0200

    exercise 4 done

---
+++ day1/exercise4.php
@@ -0,0 +1,18 @@
+<?php
+$str="Hello World !";
+$i=0;
+function while(){
+    if ($i<$str.length){
+        echo $str
+    }
+}
+?>
+
+ou 
+
+<?php
+$str="Hello World !"
+$i=0
+$arr1 = str_split($str,$i+1);
+print_r($arr1); 
+?>
