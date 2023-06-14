135c9a9d7b54bb3ab39b6ed68137b99eb704e9da
mo smock <mohamed.kanloga@gmail.com>
2023-06-14 15:49:37 +0200

    exercise 5 true

--- day1/exercise5.php
+++ day1/exercise5.php
@@ -1,9 +1,16 @@
 <?php
-$str="abcdefghijklmnopqrstuvwxyz"
-$i=0
-function while(){
-if ($i<$str.length)
-echo $str
+$str="abcdefghijklmnopqrstuvwxyz";
+$i=0;
+
+while($i<strlen($str)){
+    if ($i % 2){
+        echo $str[$i];
+    } 
+    $i=$i+1   
 }
 ?>
 
+0 % 2 = 0
+1 % 2 = 1
+2 % 2 = 0
+3 % 2 = 1
