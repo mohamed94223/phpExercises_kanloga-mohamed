3ce6f14c0c9f9332d0bf24bfa9f8ca875af14f3a
mo smock <mohamed.kanloga@gmail.com>
2023-06-15 15:04:16 +0200

    exercise 3 done

--- day2/exercise3.php
+++ day2/exercise3.php
@@ -1,14 +1,17 @@
 <?php
-$quotations=array(["Quand Paris s'enrhume,l'Europe a froid"],["S'il y a un diamant dans la poitrine, il brille sur le visage"],["La joie de vivre n'est pas un but, mais un devoir"]);
+$quotations=array("Quand Paris s'enrhume,l'Europe a froid","S'il y a un diamant dans la poitrine, il brille sur le visage","La joie de vivre n'est pas un but, mais un devoir");
 $count=0;
 
 function countingVowels($quotations,$count){
     foreach($quotations as $str){
-        for($str){
-            if($quotations[0]){}
+        for($i=0 ; $i < strlen($str);$i++){
+            if($str[$i] == "a" || $str[$i] =="e" || $str[$i] == "E"|| $str[$i] =="i" || $str[$i] =="o" || $str[$i] =="u" || $str[$i] =="y"){
+                $count++;
+            }
         }
-    }
-        
+        echo $str." : ".$count."<br/>";
+        $count=0;
     }
 }
+countingVowels($quotations,$count);
 ?>
