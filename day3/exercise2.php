39523cec0ca80d93cf8a15ac32187be81cbcf4db
mo smock <mohamed.kanloga@gmail.com>
2023-06-16 14:36:24 +0200

    exercise 2 done

--- day3/exercise2.php
+++ day3/exercise2.php
@@ -3,7 +3,10 @@ $count=0;
 $start=1;
 $end=50;
 
-while($start && $end as $count){
-    echo count($start && $end)
+for($i = $start; $i <= $end; $i++){
+    if($i % 2 === 0){
+        $count+ = $i;
+    }
 }
+echo "la somme des nombre pairs entre 1 et 50 est : ".$count;
 ?>
